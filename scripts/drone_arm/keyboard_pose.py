3a01299a5e78094fcebd9f303b2dcf5e7e09cc45
Nikita Mikhailovskiy <nmihaylovskiy@gmail.com>
2022-02-15 11:11:43 +0300

    Improved keyboard controls

--- scripts/drone_arm/keyboard_pose.py
+++ scripts/drone_arm/keyboard_pose.py
@@ -3,10 +3,12 @@ import getch
 import rospy
 from geometry_msgs.msg import PoseStamped
 from tf.transformations import *
+from px4_control.srv import UnitySetState, UnitySetStateRequest
 
 def keys():
     pub = rospy.Publisher('unity/set_pose',PoseStamped,queue_size=1)
     rospy.init_node('keypress',anonymous=True)
+    chmode = rospy.ServiceProxy('unity/set_state', UnitySetState)
     rate = rospy.Rate(10)
     pose = PoseStamped()
     pose.pose.position.x = 0
@@ -15,42 +17,42 @@ def keys():
     roll = 0
     pitch = 0
     yaw = 3.1415279/2
+    rospy.logwarn("\nControls:\n ESC - exit\n W,A,S,D - change position along X,Y\n Q,E - yaw\n Z - up, X - down\n 1 - land, 2 - takeoff, 3 - hover, 4 - position control")
     while not rospy.is_shutdown():
         k=ord(getch.getch())
-        #if ((k>=65)&(k<=68)|(k==115)|(k==113)|(k==97)):# to filter only the up , dowm ,left , right key /// this line can be removed or more key can be added to this
-        if k == 119:
-            pitch += 0.1
-        elif k == 115:
-            pitch -= 0.1
-        elif k == 97:
-            roll += 0.1
-        elif k == 100:
-            roll -= 0.1
-        elif k == 27:
-            rospy.signal_shutdown("EXIT")
-        elif k == 113:
-            yaw += 0.1
-        elif k == 101:
-            yaw -= 0.1
-        elif k == 105:
+        
+        if k == 119: #W
             pose.pose.position.x += 0.1
-        elif k == 107:
+        elif k == 115: #S
             pose.pose.position.x -= 0.1
-        elif k == 106:
+        elif k == 97: #A
             pose.pose.position.y += 0.1
-        elif k == 108:
+        elif k == 100: #D
             pose.pose.position.y -= 0.1
-        elif k == 122:
+        elif k == 27: #ESC
+            rospy.signal_shutdown("EXIT")
+        elif k == 113: #Q
+            yaw += 0.1
+        elif k == 101: #E
+            yaw -= 0.1
+        elif k == 122: #X
             pose.pose.position.z += 0.1
-        elif k == 120:
-            pose.pose.position.z -= 0.1    
+        elif k == 120: #Z
+            pose.pose.position.z -= 0.1   
+        elif k == 49: #1
+            chmode(UnitySetStateRequest.LAND)
+        elif k == 50: #2
+            chmode(UnitySetStateRequest.TAKEOFF)
+        elif k == 51: #3
+            chmode(UnitySetStateRequest.HOVER)
+        elif k == 52: #4
+            chmode(UnitySetStateRequest.POSCTL)
         q = quaternion_from_euler(roll, pitch, yaw)
         pose.header.stamp = rospy.Time.now()
         pose.pose.orientation.x = q[0]
         pose.pose.orientation.y = q[1]
         pose.pose.orientation.z = q[2]
         pose.pose.orientation.w = q[3]
-        print(pose)
         pub.publish(pose)#to publish
         rate.sleep()
 
